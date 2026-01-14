e412d3aee1dda81ae2fd9dc017e2259fd96d297b
Melany Palacio <melany.palacio@l4ventures.com>
2026-01-14 00:48:21 -0500

    Eliminar referencia a Selenium en pruebas automatizadas.

--- src/Components/About/index.jsx
+++ src/Components/About/index.jsx
@@ -5,7 +5,7 @@ const About = () => {
     <div className='about'>
       <h1 className='about__title'>About</h1>
       <p className='about__description'>
-    I am a <b>Full Stack Software Developer</b> and <b>QA Analyst</b>, with a degree in <b>Computer Engineering</b> from Politécnico Colombiano Jaime Isaza Cadavid. I specialize in building scalable web applications using <b>JavaScript, TypeScript, React, and Java</b>, while ensuring high-quality standards through <b>manual testing</b> and <b>automated testing with Selenium</b>.
+    I am a <b>Full Stack Software Developer</b> and <b>QA Analyst</b>, with a degree in <b>Computer Engineering</b> from Politécnico Colombiano Jaime Isaza Cadavid. I specialize in building scalable web applications using <b>JavaScript, TypeScript, React, and Java</b>, while ensuring high-quality standards through <b>manual testing</b> and <b>automated testing</b>.
     <br /><br />
     I am passionate about software quality, clean code, and effective collaboration in multidisciplinary teams, working under the <b>Scrum methodology</b>. My curiosity and commitment to continuous learning have led me to actively engage with the tech community as part of <b>PionerasDev</b> and attend events such as MedellínCSS, CSSConfColombia, JSConfCO, and more.
     <br /><br />
